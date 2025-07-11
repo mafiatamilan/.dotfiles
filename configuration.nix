bee156756ed06aff2878e0921d5b032ec7a70331
Yogs # <mafiatamilan@yahoo.com>
2025-07-11 21:17:43 +0530

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -3,11 +3,17 @@
 {
   imports = [ ./hardware-configuration.nix ];
 
+  ########################################
+  # Boot Configuration
+  ########################################
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.devices = [ "nodev" ];
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.useOSProber = true;
+
+  boot.loader.grub = {
+    enable = true;
+    devices = [ "nodev" ];
+    efiSupport = true;
+    useOSProber = true;
+  };
 
   boot.kernelPatches = [
     {
@@ -16,42 +22,57 @@
     }
   ];
 
-  # 🧠 Load mic driver and firmware
   boot.kernelModules = [ "snd_pci_acp6x" ];
   hardware.firmware = [ pkgs.firmwareLinuxNonfree ];
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
 
-  networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
+  ########################################
+  # Networking
+  ########################################
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+  };
 
+  ########################################
+  # Localization and Time
+  ########################################
   time.timeZone = "Asia/Kolkata";
+
   i18n.defaultLocale = "en_IN";
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_IN";
+    LC_ADDRESS        = "en_IN";
     LC_IDENTIFICATION = "en_IN";
-    LC_MEASUREMENT = "en_IN";
-    LC_MONETARY = "en_IN";
-    LC_NAME = "en_IN";
-    LC_NUMERIC = "en_IN";
-    LC_PAPER = "en_IN";
-    LC_TELEPHONE = "en_IN";
-    LC_TIME = "en_IN";
+    LC_MEASUREMENT    = "en_IN";
+    LC_MONETARY       = "en_IN";
+    LC_NAME           = "en_IN";
+    LC_NUMERIC        = "en_IN";
+    LC_PAPER          = "en_IN";
+    LC_TELEPHONE      = "en_IN";
+    LC_TIME           = "en_IN";
   };
 
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
+  ########################################
+  # Display and Desktop Environment
+  ########################################
+  services.xserver = {
+    enable = true;
+    desktopManager.plasma6.enable = true;
+    xkb.layout = "us";
+    videoDrivers = [ "nvidia" ];
   };
+  services.displayManager.sddm.enable = true;
 
-  services.xserver.videoDrivers = [ "nvidia" ];
+  ########################################
+  # NVIDIA and AMD GPU Configuration
+  ########################################
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
     open = false;
     nvidiaSettings = true;
+
     prime = {
       offload = {
         enable = true;
@@ -64,11 +85,17 @@
 
   hardware.amdgpu.amdvlk.enable = true;
 
+  ########################################
+  # Printing
+  ########################################
   services.printing.enable = true;
 
-  # 🎧 Audio configuration with modern UCM override
+  ########################################
+  # Audio Configuration
+  ########################################
   hardware.alsa.enable = true;
   services.pulseaudio.enable = false;
+
   security.rtkit.enable = true;
 
   services.pipewire = {
@@ -78,54 +105,105 @@
     pulse.enable = true;
     jack.enable = true;
 
-    # ✅ correct UCM override method
     extraConfig.pipewire."context.properties" = {
       "alsa.use-ucm" = false;
     };
   };
 
+  ########################################
+  # User Configuration
+  ########################################
   users.users.yogs = {
     isNormalUser = true;
     description = "Yogs";
+    shell = pkgs.zsh;
     extraGroups = [ "networkmanager" "wheel" "audio" ];
+
     packages = with pkgs; [
       firefox
       obs-studio
       neovim
       steam
       spotify
-      gcc
-      vscode
-      android-tools
-      python310Full
-      git
-      gh
       discord
-      go
-      flex
-      bison
-      ncurses
-      gnumake42
-      bc
+      alacritty
+      cmake
+      qt6.qtbase
+      qt6.full
+      qtcreator
+      telegram-desktop
     ];
   };
 
-  programs.appimage.enable = true;
-  programs.appimage.binfmt = true;
+  ########################################
+  # Zsh and Powerlevel10k Configuration
+  ########################################
+  programs.zsh = {
+    enable = true;
+    enableBashCompletion = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+  };
+  programs.zsh.ohMyZsh = {
+    enable = true;
+    plugins = [ "git" ];
+    custom = "$HOME/.oh-my-zsh/custom/";
+    theme = "powerlevel10k/powerlevel10k";
+  };
+
+  ########################################
+  # Misc Programs and Features
+  ########################################
+  programs.appimage = {
+    enable = true;
+    binfmt = true;
+  };
 
   programs.firefox.enable = true;
-  programs.steam.enable = true;
-  programs.steam.gamescopeSession.enable = true;
+
+  programs.steam = {
+    enable = true;
+    gamescopeSession.enable = true;
+  };
+
   programs.gamemode.enable = true;
 
-  nixpkgs.config.allowUnfree = true;
+  ########################################
+  # Global Environment Variables for Qt6
+  ########################################
+  environment.variables = {
+    QT_PLUGIN_PATH = "${pkgs.qt6.qtbase}/lib/qt-6/plugins";
+    QML2_IMPORT_PATH = "${pkgs.qt6.qtdeclarative}/lib/qt-6/qml";
+    PATH = "${pkgs.qt6.qtbase}/bin:${pkgs.qt6.qttools}/bin:${pkgs.qtcreator}/bin:\${PATH}";
+  };
 
+  ########################################
+  # System Packages and Options
+  ########################################
   environment.systemPackages = with pkgs; [
+    zsh
+    zsh-powerlevel10k
+
+    # Dev tools
+    android-tools
+    git
+    gh
+    go
+    tmux
+    cmake
+    gcc
+    gdb
+    python310Full
+
     pciutils
     usbutils
     inxi
   ];
 
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+  ];
+
+  nixpkgs.config.allowUnfree = true;
   system.stateVersion = "25.05";
 }
-
